9a10951701102f3c37d324ea836a99f737cc3e8e
Willians Augusto <wiliansaugusto@gmail.com>
2021-05-16 15:23:25 -0300

    Update test.

--- src/main/java/one/digital/inovation/peopleregistrerapi/controller/PersonController.java
+++ src/main/java/one/digital/inovation/peopleregistrerapi/controller/PersonController.java
@@ -8,9 +8,6 @@ import one.digital.inovation.peopleregistrerapi.exception.PersonNotFoundExceptio
 import one.digital.inovation.peopleregistrerapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +17,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
+import javax.validation.Valid;
+import java.util.List
     @RestController
     public class PersonController {
 
